88a8f072dbb8a8ea252c357f02ce60259f50bd01
kim-iths <kim.hellman@iths.se>
2021-07-28 17:20:25 +0200

    Added a temporary reset button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-// import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Game from './components/Game'
+import { useCookies } from "react-cookie"
 
 function App() {
+
+  const [cookies, setCookie, removeCookie] = useCookies([])
+  
+  //Clear cookies and reload page
+  function reset(){
+    for(const c in cookies){
+      removeCookie(c.toString())
+    }
+    window.location.reload()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h3>rgb</h3>
+        <button className="reset" onClick={reset}><h3>reset</h3></button>
       </header>
       <main>
         <Game />
