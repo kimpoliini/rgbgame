73a65c654e034d9002e2be09adaf30b4cd3b7b00
kim <kim.hellman@iths.se>
2022-04-13 03:14:09 +0200

    Added ranks to some upgrades, making them purchasable multiple times

--- src/components/Upgrade.jsx
+++ src/components/Upgrade.jsx
@@ -2,14 +2,16 @@ import "./styles/sideMenuItems.css"
 import { upgrades } from "../js/data/upgrades"
 import { useState } from "react"
 
-const Upgrade = ({onClick, upgradeId}) => {
+const Upgrade = ({ onClick, upgradeId }) => {
 
     const [upgrade, setUpgrade] = useState(upgrades[upgradeId])
 
-    return(
+    return (
         <div id={`upgrade-${upgradeId}`} className="side-menu-item cannot-afford" onClick={onClick}>
             <div>
-                <h5>{upgrade.name}</h5>
+                <h5>{upgrade.name + (upgrade.maxRanks ?
+                    (upgrade.rank ? " " + (upgrade.rank + 1) : " 1")
+                    : "")}</h5>
                 <p>{upgrade.description}</p>
                 <div className="generator-price">
                     <span>{upgrade.price[0]}</span>
