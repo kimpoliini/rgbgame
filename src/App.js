0192f17d79f3dcaf77b761bfafd0297e67037728
kimpoliini <kimpas@hotmail.se>
2025-01-30 02:56:14 +0100

    Changed title, added Kofi component, cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Game from './components/Game'
-import { useCookies } from "react-cookie"
+import Kofi from './components/Kofi'
 import { useState } from 'react';
 import Options from './components/Options';
 
@@ -8,8 +8,8 @@ function App() {
 
   const [isOptionsOpen, setIsOptionsOpen] = useState(false)
 
-  function toggleOptions(){
-    if(isOptionsOpen){
+  function toggleOptions() {
+    if (isOptionsOpen) {
       document.querySelector(".options-background").classList.add("on-remove")
       setTimeout(() => {
         setIsOptionsOpen(!isOptionsOpen)
@@ -22,11 +22,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3 className="title">rgb</h3>
-        <button className="header-button" onClick={toggleOptions}><h3>options</h3></button>
+        <h3 className="title">RGB Inc</h3>
+        <Kofi />
+        <button className="header-button" onClick={toggleOptions}><h3>Options</h3></button>
       </header>
       <main>
-        {isOptionsOpen ? <Options  dismiss={toggleOptions}/> : null}
+        {isOptionsOpen ? <Options dismiss={toggleOptions} /> : null}
         <Game />
       </main>
     </div>
---
+++ src/components/Kofi.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Kofi = () => {
+    return (
+        <a href='https://ko-fi.com/E1E1JWRE0' target='_blank' rel="noreferrer">
+          <img
+            height='36' style={{ border: 0, height: 36, marginRight: "1rem" }}
+            src='https://storage.ko-fi.com/cdn/kofi5.png?v=6'
+            alt='Buy Me a Coffee at ko-fi.com' />
+        </a>
+    );
+};
+
+export default Kofi;
